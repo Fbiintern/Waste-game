f05523b8445f84760ffea7c2b0ecfd3024286096
314yush <startrek2219@gmail.com>
2024-10-13 15:38:41 +0530

    image, css tweaks

--- config/Provider.tsx
+++ config/Provider.tsx
@@ -25,7 +25,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
           walletList: ["detected_wallets", "rabby_wallet","coinbase_wallet", "rainbow"],
           theme: "light",
           accentColor: "#676FFF",
-          logo: "https://your-logo-url",
+          logo: "https://github.com/314yush/this-damn-game/blob/main/public/Frame%201321315999.png?raw=true",
         },
         embeddedWallets: {
           createOnLogin: "users-without-wallets",
